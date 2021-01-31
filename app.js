59785697978b19b5b039208fad43a32bf94c5a11
Peters0n <petersonflemm@gmail.com>
2021-01-31 14:19:12 -0400

    fix server

--- app.js
+++ app.js
@@ -9,20 +9,18 @@ app.use(express.static(publicPath) );
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
-})
+});
 
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'))
-})
+});
 
 
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'))
-})
+});
 
-app.listen(3000, () => {
-    console.log('server ready')
-})
-process.env.PORT || 3001, function() {
-    console.log('Servidor corriendo')
-}
+
+app.listen(process.env.PORT || 3001, function() {
+    console.log('Servidor corriendo');
+});
